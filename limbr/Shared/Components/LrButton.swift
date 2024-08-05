7906628d0ef94b2136143ae4481b250291ce0882
Ngurah Surya <ngurahgusti584@gmail.com>
2024-08-05 12:03:32 +0700

    feat: add two component file, button and container

--- limbr/Shared/Components/LrButton.swift
+++ limbr/Shared/Components/LrButton.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class LrButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String){
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure(){
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 20
+    }
+    
 }
---
+++ limbr/Shared/Components/LrContainerView.swift
@@ -0,0 +1,33 @@
+//
+//  LrContainerView.swift
+//  limbr
+//
+//  Created by I Gusti Ngurah Surya Ardika Dinataputra on 05/08/24.
+//
+
+import UIKit
+
+class LrContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(){
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    private func configure(){
+        translatesAutoresizingMaskIntoConstraints   = false
+        layer.backgroundColor                       = UIColor(named: "containerBackground")?.cgColor
+        layer.cornerRadius                          = 20
+        layer.borderColor                           = UIColor(named: "containerBackground")?.cgColor
+    }
+    
+}
