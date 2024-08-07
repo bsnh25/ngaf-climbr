bc5b2dfe001f60c88cc09dd2c411d7fe9c84db15
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-08-08 05:43:45 +0700

    Initial Commit

---
+++ climbr/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  climbr
+//
+//  Created by Ivan Nur Ilham Syah on 08/08/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
