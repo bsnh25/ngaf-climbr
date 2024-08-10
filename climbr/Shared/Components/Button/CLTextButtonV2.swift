1e4db231eb4d9ec8045c7d7aead4b05c24b0baa3
Ngurah Surya <ngurahgusti584@gmail.com>
2024-08-10 14:44:08 +0700

    fix: fixing update layer into component CLTextButtonV2 to make it have click animation

--- climbr/Shared/Components/Button/CLTextButtonV2.swift
+++ climbr/Shared/Components/Button/CLTextButtonV2.swift
@@ -9,35 +9,26 @@ import Cocoa
 
 class CLTextButtonV2: NSButton {
 
+
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         self.isBordered = false // Remove default button border
+        self.wantsLayer = true // Ensure the layer is available for custom drawing
     }
-    
-    override func draw(_ dirtyRect: NSRect) {
-        if let backgroundColor = layer?.backgroundColor {
-            NSColor(cgColor: backgroundColor)?.setFill()
-        } else {
-            NSColor.clear.setFill()
-        }
-        let path = NSBezierPath(roundedRect: bounds, xRadius: 20, yRadius: 20)
-        path.fill()
-        super.draw(dirtyRect)
-    }
-    
+
     init(title: String, backgroundColor: NSColor, foregroundColorText: NSColor, fontText: NSFont) {
         super.init(frame: .zero)
         wantsLayer = true
         self.title = title
-        self.isBordered = false // Remove default button border
-        self.bezelStyle = .regularSquare // Set the bezel style to a flat square
+        self.isBordered = false
+        self.bezelStyle = .regularSquare
         configure(foregroundColorText: foregroundColorText, font: fontText, backgroundColor: backgroundColor)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func configure(foregroundColorText: NSColor, font: NSFont, backgroundColor: NSColor) {
         let attributes: [NSAttributedString.Key: Any] = [
             .foregroundColor: foregroundColorText,
@@ -47,6 +38,16 @@ class CLTextButtonV2: NSButton {
         layer?.backgroundColor = backgroundColor.cgColor
         attributedTitle = NSAttributedString(string: self.title, attributes: attributes)
         translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    override func updateLayer() {
+        super.updateLayer()
+
+        if isHighlighted {
+            layer?.backgroundColor = layer?.backgroundColor?.copy(alpha: 0.7)
+        } else {
+            layer?.backgroundColor = layer?.backgroundColor?.copy(alpha: 1.0)
+        }
     }
     
 }
