a945970754dd7ef182cc00bb4591aae184aadea7
Fatih <fatakhillah.khaqo@gmail.com>
2024-08-11 00:19:06 +0700

    feat: add camera manager and predictor

--- climbr/Managers/CameraManager.swift
+++ climbr/Managers/CameraManager.swift
@@ -5,8 +5,72 @@
 //  Created by Fatakhillah Khaqo on 08/08/24.
 //
 
-import Foundation
+import AVFoundation
+import AppKit
 
-class CameraManager {
-    //MARK: TODO 
+class CameraManager: NSObject {
+    let captureSession: AVCaptureSession
+    let videoOutput: AVCaptureVideoDataOutput
+    let captureDevice: AVCaptureDevice?
+    
+    var predictor = Predictor()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    override init() {
+        captureSession = AVCaptureSession()
+        videoOutput = AVCaptureVideoDataOutput()
+        captureDevice = AVCaptureDevice.default(for: .video)
+        
+        super.init()
+        
+        setupSession()
+    }
+    
+    private func setupSession() {
+        guard let captureDevice = captureDevice,
+              let input = try? AVCaptureDeviceInput(device: captureDevice) else {
+            return
+        }
+        
+        captureSession.sessionPreset = .high
+        captureSession.addInput(input)
+        
+        videoOutput.alwaysDiscardsLateVideoFrames = true
+        captureSession.addOutput(videoOutput)
+        
+        setupPreviewLayer()
+    }
+    
+    private func setupPreviewLayer() {
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = .resizeAspectFill
+    }
+    
+    func startSession() {
+        guard !captureSession.isRunning else { return }
+        
+        captureSession.startRunning()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoDispatchQueue"))
+    }
+    
+    func stopSession() {
+        guard captureSession.isRunning else { return }
+        
+        captureSession.stopRunning()
+    }
+    
+    func attachPreview(to view: NSView) {
+        guard let previewLayer = previewLayer else { return }
+        
+        previewLayer.frame = view.bounds
+        view.wantsLayer = true
+        view.layer?.addSublayer(previewLayer)
+    }
 }
+
+extension CameraManager: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        predictor.estimation(sampleBuffer: sampleBuffer)
+    }
+}
+
