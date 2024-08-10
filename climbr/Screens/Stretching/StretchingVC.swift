21cb90de471118b7f479a4c0e8d3e3fd4106f0e0
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-08-10 22:36:42 +0700

    fix: change variable and function name, add docs

--- climbr/Screens/Stretching/StretchingVC.swift
+++ climbr/Screens/Stretching/StretchingVC.swift
@@ -10,7 +10,7 @@ import Combine
 
 class StretchingVC: NSViewController {
     let cameraPreview           = NSView()
-    let excerciseInfoView       = NSView()
+    let movementInfoView        = NSView()
     let currentMovementView     = CurrentMovementView()
     let nextMovementView        = NextMovementView()
     let skipButton              = CLTextButtonV2(title: "Skip", backgroundColor: .black, foregroundColorText: .white, fontText: .boldSystemFont(ofSize: 16))
@@ -28,11 +28,11 @@ class StretchingVC: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureExcerciseInfoView()
+        configureMovementView()
         configureCameraPreview()
-        configureStack()
         configureButton()
         
+        /// Stream the current index and update on its changed
         $currentIndex.sink { index in
             let movement = Movement.items[index]
             
@@ -40,6 +40,7 @@ class StretchingVC: NSViewController {
         }
         .store(in: &bags)
         
+        /// Stream the next index and update on its changed
         $nextIndex.sink { index in
             let movement = Movement.items[index]
             
@@ -47,34 +48,6 @@ class StretchingVC: NSViewController {
         }
         .store(in: &bags)
 
-    }
-    
-    private func configureStack() {
-        /// Create divider
-        let divider             = Divider()
-        
-        /// Arrange video preview, divider, and next excercise vertically
-        let stack               = NSStackView(views: [currentMovementView, divider, nextMovementView])
-        stack.orientation       = .vertical
-        stack.spacing           = 24
-        stack.alignment         = .leading
-        
-        view.addSubview(stack)
-        
-        NSLayoutConstraint.activate([
-            stack.topAnchor.constraint(equalTo: excerciseInfoView.topAnchor, constant: padding),
-            stack.leadingAnchor.constraint(equalTo: excerciseInfoView.leadingAnchor, constant: padding),
-            stack.trailingAnchor.constraint(equalTo: excerciseInfoView.trailingAnchor, constant: -padding),
-            
-            currentMovementView.leadingAnchor.constraint(equalTo: stack.leadingAnchor),
-            currentMovementView.trailingAnchor.constraint(equalTo: stack.trailingAnchor),
-            
-            nextMovementView.leadingAnchor.constraint(equalTo: stack.leadingAnchor),
-            nextMovementView.trailingAnchor.constraint(equalTo: stack.trailingAnchor),
-            
-            divider.leadingAnchor.constraint(equalTo: stack.leadingAnchor),
-            divider.trailingAnchor.constraint(equalTo: stack.trailingAnchor),
-        ])
     }
     
     private func configureCameraPreview() {
@@ -94,30 +67,63 @@ class StretchingVC: NSViewController {
             cameraPreview.topAnchor.constraint(equalTo: view.topAnchor),
             cameraPreview.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             cameraPreview.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            cameraPreview.trailingAnchor.constraint(equalTo: excerciseInfoView.leadingAnchor),
+            cameraPreview.trailingAnchor.constraint(equalTo: movementInfoView.leadingAnchor),
             
             sampleText.centerXAnchor.constraint(equalTo: cameraPreview.centerXAnchor),
             sampleText.centerYAnchor.constraint(equalTo: cameraPreview.centerYAnchor),
         ])
     }
     
-    private func configureExcerciseInfoView() {
-        view.addSubview(excerciseInfoView)
+    /// Configure the movement sidebar info
+    ///
+    /// Set the background to white, width equal to 0.3 of the window width
+    private func configureMovementView() {
+        view.addSubview(movementInfoView)
         
-        excerciseInfoView.translatesAutoresizingMaskIntoConstraints = false
-        excerciseInfoView.wantsLayer                = true
-        excerciseInfoView.layer?.backgroundColor    = .white
+        movementInfoView.translatesAutoresizingMaskIntoConstraints = false
+        movementInfoView.wantsLayer                = true
+        movementInfoView.layer?.backgroundColor    = .white
         
         NSLayoutConstraint.activate([
-            excerciseInfoView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            excerciseInfoView.topAnchor.constraint(equalTo: view.topAnchor),
-            excerciseInfoView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            excerciseInfoView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.3),
+            movementInfoView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            movementInfoView.topAnchor.constraint(equalTo: view.topAnchor),
+            movementInfoView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            movementInfoView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.3),
+        ])
+        
+        configureMovementStack()
+    }
+    
+    private func configureMovementStack() {
+        /// Create divider
+        let divider             = Divider()
+        
+        /// Arrange current movement view, divider, and next movement vertically
+        let stack               = NSStackView(views: [currentMovementView, divider, nextMovementView])
+        stack.orientation       = .vertical
+        stack.spacing           = 24
+        stack.alignment         = .leading
+        
+        view.addSubview(stack)
+        
+        NSLayoutConstraint.activate([
+            stack.topAnchor.constraint(equalTo: movementInfoView.topAnchor, constant: padding),
+            stack.leadingAnchor.constraint(equalTo: movementInfoView.leadingAnchor, constant: padding),
+            stack.trailingAnchor.constraint(equalTo: movementInfoView.trailingAnchor, constant: -padding),
+            
+            currentMovementView.leadingAnchor.constraint(equalTo: stack.leadingAnchor),
+            currentMovementView.trailingAnchor.constraint(equalTo: stack.trailingAnchor),
+            
+            nextMovementView.leadingAnchor.constraint(equalTo: stack.leadingAnchor),
+            nextMovementView.trailingAnchor.constraint(equalTo: stack.trailingAnchor),
+            
+            divider.leadingAnchor.constraint(equalTo: stack.leadingAnchor),
+            divider.trailingAnchor.constraint(equalTo: stack.trailingAnchor),
         ])
     }
     
+    /// Configure button horizontally
     private func configureButton() {
-        
         let divider                 = Divider()
         let buttonStack             = NSStackView(views: [skipButton, finishButton])
         
@@ -140,15 +146,15 @@ class StretchingVC: NSViewController {
         finishButton.action = #selector(skip)
         
         NSLayoutConstraint.activate([
-            buttonStack.leadingAnchor.constraint(equalTo: excerciseInfoView.leadingAnchor, constant: padding),
-            buttonStack.bottomAnchor.constraint(equalTo: excerciseInfoView.bottomAnchor, constant: -padding),
-            buttonStack.trailingAnchor.constraint(equalTo: excerciseInfoView.trailingAnchor, constant:  -padding),
+            buttonStack.leadingAnchor.constraint(equalTo: movementInfoView.leadingAnchor, constant: padding),
+            buttonStack.bottomAnchor.constraint(equalTo: movementInfoView.bottomAnchor, constant: -padding),
+            buttonStack.trailingAnchor.constraint(equalTo: movementInfoView.trailingAnchor, constant:  -padding),
             
             skipButton.heightAnchor.constraint(equalToConstant: 48),
             finishButton.heightAnchor.constraint(equalToConstant: 48),
             
-            divider.leadingAnchor.constraint(equalTo: excerciseInfoView.leadingAnchor, constant: padding),
-            divider.trailingAnchor.constraint(equalTo: excerciseInfoView.trailingAnchor, constant: -padding),
+            divider.leadingAnchor.constraint(equalTo: movementInfoView.leadingAnchor, constant: padding),
+            divider.trailingAnchor.constraint(equalTo: movementInfoView.trailingAnchor, constant: -padding),
             divider.bottomAnchor.constraint(equalTo: buttonStack.topAnchor, constant: -padding),
         ])
     }
