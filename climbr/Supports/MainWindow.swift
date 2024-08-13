37dd58272954288c4859c584cb0d929bce627fcf
Fatih <fatakhillah.khaqo@gmail.com>
2024-08-13 13:49:52 +0700

    feat: building the ShopVC

--- climbr/Supports/MainWindow.swift
+++ climbr/Supports/MainWindow.swift
@@ -26,7 +26,7 @@ class MainWindow: NSWindow {
         
         /// Set the window to center axis by default
         center()
-        let vc                  = HomeVC()
+        let vc                  = UserPreferenceVC()
         contentView             = vc.view
         contentViewController   = vc
     }
