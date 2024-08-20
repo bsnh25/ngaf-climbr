cbd6157fbcae3a1b663aebb0e69482a9dd57a5e2
Bayu Septyan <bayuseptyan925@gmail.com>
2024-08-20 17:12:53 +0700

    fix!: dock and status bar icon

--- climbr/Supports/AppDelegate.swift
+++ climbr/Supports/AppDelegate.swift
@@ -8,7 +8,7 @@
 import AppKit
 import Swinject
 
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
 
     var mainWindow: MainWindow?
     var statusBar: NSStatusBar!
@@ -21,7 +21,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to initialize your application
         mainWindow = MainWindow()
         
+        NSApp.setActivationPolicy(.regular)
+        NSApplication.shared.setActivationPolicy(.regular)
+        
         guard let mainWindow = mainWindow else {return}
+        mainWindow.delegate = self
 
         /// Create status bar instance
         statusBar       = NSStatusBar()
@@ -43,13 +47,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
         NSApp.appearance = NSAppearance(named: .aqua)
         ///audio setup
-        audio?.playBackgroundMusic(fileName: "bgmusic")
+        if let audio = audio {
+            audio.playBackgroundMusic(fileName: "bgmusic")
+        } else {
+            print("AudioService not resolved.")
+        }
+
         mainWindow.makeKeyAndOrderFront(nil)
     }
     
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        audio?.stopBackground()
+        print("Application will terminate")
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
@@ -62,15 +73,51 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         if let window = NSApplication.shared.windows.first {
             window.makeKeyAndOrderFront(nil)
             NSApplication.shared.activate(ignoringOtherApps: true)
+        } else {
+            print("Main window is not available.")
         }
     }
     
     
     func applicationDidResignActive(_ notification: Notification) {
         audio?.stopBackground()
-        print("Mati")
+        print("Application resigned active")
+        
     }
 
+    func windowWillClose(_ notification: Notification) {
+        NSApp.hide(nil) // Menyembunyikan aplikasi saat jendela ditutup
+        if let audio = audio {
+            audio.stopBackground()
+        } else {
+            print("AudioService not resolved.")
+        }
 
+        print("Application will close")
+        mainWindow?.isReleasedWhenClosed = false
+        NSApplication.shared.setActivationPolicy(.accessory)
+    }
+    
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+
+        
+        if !flag {
+            mainWindow?.orderFront(self)
+            if let window = mainWindow {
+                window.makeKeyAndOrderFront(nil)
+                NSApplication.shared.activate(ignoringOtherApps: true)
+            } else {
+                mainWindow?.makeKeyAndOrderFront(self)
+                print("Creating a new mainWindow instance.")
+                mainWindow = MainWindow()
+                mainWindow?.delegate = self
+                mainWindow?.makeKeyAndOrderFront(nil)
+                NSApplication.shared.activate(ignoringOtherApps: true)
+            }
+        }
+        
+        return true
+    }
 }
 
