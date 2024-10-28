4e53fe6ca9fc5ad37b5cda29795de66700fc3235
Ilham Syah <42111838+hans-nullpt@users.noreply.github.com>
2024-10-28 16:45:47 +0700

    NGAF-300 Feature/streak (#13)
    * fix: change provisioning profile
    
    * feat: create streak entity for core data
    
    * feat: create get and store streak function
    
    * feat: add DS_Store to gitignore

---
+++ climbr/Models/Streak.swift
@@ -0,0 +1,14 @@
+//
+//  Streak.swift
+//  climbr
+//
+//  Created by Ivan Nur Ilham Syah on 28/10/24.
+//
+
+import Foundation
+
+struct Streak {
+  var date: Date
+  var completedSession: Int
+  var collectedEquipmentId: Int
+}
