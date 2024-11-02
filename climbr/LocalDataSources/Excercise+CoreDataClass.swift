6cff8642b2949a85a35aee37da2f9a6ea5dbfdfb
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-11-02 15:20:50 +0700

    feat: remove unused files, remove unused coredata entity

---
+++ climbr/LocalDataSources/Excercise+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Excercise+CoreDataClass.swift
+//  climbr
+//
+//  Created by Ivan Nur Ilham Syah on 02/11/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Excercise)
+public class Excercise: NSManagedObject {
+
+}
