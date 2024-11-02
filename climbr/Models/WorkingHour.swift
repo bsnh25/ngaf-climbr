7a649ea86ab05730f3898442e4ef1674c777a3cd
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-11-02 13:56:13 +0700

    feat: add isEnabled prop

--- climbr/Models/WorkingHour.swift
+++ climbr/Models/WorkingHour.swift
@@ -15,12 +15,41 @@ enum Weekday: Int, CaseIterable {
   case friday
   case saturday
   case sunday
+  
+  var fullName: String {
+    let calendar = Calendar.current
+    
+    let formatter = DateFormatter()
+    formatter.dateFormat = "EEEE"
+    
+    var components = DateComponents()
+    components.weekday = self.rawValue
+    
+    let date = Calendar.current.date(from: components)!
+    
+    return formatter.string(from: date)
+  }
+  
+  var abbreviatedName: String {
+    let calendar = Calendar.current
+    
+    let formatter = DateFormatter()
+    formatter.dateFormat = "E"
+    
+    var components = DateComponents()
+    components.weekday = self.rawValue
+    
+    let date = Calendar.current.date(from: components)!
+    
+    return formatter.string(from: date)
+  }
 }
 
 struct WorkingHour: Codable, Hashable {
-    var startHour: Date
-    var endHour: Date
-    var day: Int
+  var startHour: Date
+  var endHour: Date
+  var day: Int
+  var isEnabled: Bool = false
   
   static func ==(lhs: WorkingHour, rhs: WorkingHour) -> Bool {
     lhs.day == rhs.day
