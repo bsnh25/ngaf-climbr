889453619304f27192a5b295411581408c073ead
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-11-02 15:35:37 +0700

    feat: fix file references

---
+++ climbr/Models/LocalDataSources/Equipment+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Equipment+CoreDataClass.swift
+//  climbr
+//
+//  Created by Bayu Septyan Nur Hidayat on 16/08/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Equipment)
+public class Equipment: NSManagedObject {
+
+}
