0c39046b72ac1fb65e29a0a008c5aabc5f45626f
Bayu Septyan <bayuseptyan925@gmail.com>
2024-11-19 15:15:14 +0700

    fix: border

--- climbr/Screens/Shop/SubViews/CollectionContainerView.swift
+++ climbr/Screens/Shop/SubViews/CollectionContainerView.swift
@@ -70,8 +70,8 @@ class CollectionContainerView: NSView {
         self.layer?.cornerRadius = 10
         self.layer?.masksToBounds = true
         self.layer?.backgroundColor = .white
-        self.layer?.borderColor = NSColor.red.cgColor
-        self.layer?.borderWidth = 1
+//        self.layer?.borderColor = NSColor.red.cgColor
+//        self.layer?.borderWidth = 1
         
         setupConstraints()
         configOutterStack()
